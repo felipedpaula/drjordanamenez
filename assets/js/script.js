e87e74a2518b010fa18828fffd73097fd56c612e
Felipe Palmeira <felipedpaula97@gmail.com>
2023-05-23 14:57:16 -0300

    header | menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let btnMenu = document.querySelector('.btn-menu-mobile');
+    let menuMobile = document.querySelector('.menu-mobile'); // Corrigido o seletor aqui
+    let flagMenuMobile = 0;
+  
+    btnMenu.addEventListener('click', () => {
+        if (flagMenuMobile == 0) {
+            menuMobile.style.right = '0px';
+            flagMenuMobile = 1;
+        } else {
+            menuMobile.style.right = '-500px';
+            flagMenuMobile = 0;
+        }
+    });
+});
+  
+  
+  
+  
+  
+  
+  
